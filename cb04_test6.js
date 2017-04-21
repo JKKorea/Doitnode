dfea2ada18610cac0f5e910d6ad69f7dc0992a84
jkkorea <jongkun7987@gmail.com>
2017-04-22 05:51:33 +0900

    delete cb04_test6.js

--- cb04_test6.js
+++
@@ -1,8 +0,0 @@
-// ERROR : throw err
-var fs = require('fs');
-
-fs.readFile('./package.json', 'utf8', function(err, data) {
-  if(err) throw err;
-    // the data is passed to the callback in the second argument
-  console.log(data);
-});
