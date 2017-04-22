d976e56f88f3383b786badf2921648f67b8ce6a9
jkkorea <jongkun7987@gmail.com>
2017-04-23 06:24:46 +0900

    webserver

---
+++ ch05_test1.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer();
+console.log(server);
+
+var host = 'localhost';
+var port = 3000;
+server.listen(port, host, '50000', function(){
+    console.log('웹서버가 실행되었습니다 -> ' + host + ':' + port);
+});
