46e7b07dbed95737321a3d989fadfda88972eada
jkkorea <jongkun7987@gmail.com>
2017-04-30 04:29:56 +0900

    cookieParser

--- ExpressExample/app11.js
+++ ExpressExample/app11.js
@@ -4,6 +4,7 @@ var static = require('serve-static');
 var path = require('path');
 
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
@@ -13,16 +14,40 @@ app.use('/public', static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 var router = express.Router();
 
-router.route('/process/login').post(function(req, res){
+router.route('/process/setUserCookie').get(function(req, res){
+    console.log('/process/setUserCookie 라우팅 함수 호출됨.');
+    
+    res.cookie('user', {
+        id:'mike',
+        name:'소녀시대',
+        authorized:true
+    });
+    
+    res.redirect('/process/showCookie');
+});
+
+router.route('/process/showCookie').get(function(req, res){
+    console.log('/process/showCookie 라우팅 함수 호출됨');
+    
+    res.send(req.cookies);
+});
+
+
+router.route('/process/login/:name').post(function(req, res){
     console.log('/process/login 라우팅 함수에서 받음.');
+    
+    var paramName = req.params.name;
     
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
     
     res.writeHead(200, {"Content-Type":"text/html; charset=utf8"});
     res.write("<h1>서버에서 로그인 응답</h1>");
+    res.write("<div><p>" + paramName +"</p></div>");
     res.write("<div><p>" + paramId+"</p></div>");
     res.write("<div><p>" + paramPassword+"</p></div>");
     res.end();
